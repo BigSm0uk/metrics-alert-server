7786446dacff7ac3e0886c2727e021f4a865b280
Denis Dekke <88782528+BigSm0uk@users.noreply.github.com>
2025-11-25 10:46:30 +0300

    Инкремент 23

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printBuildInfo()
+
 	app, err := initializeApp()
 	if err != nil {
 		log.Fatal(err)
@@ -13,4 +22,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printBuildInfo() {
+	version := buildVersion
+	if version == "" {
+		version = "N/A"
+	}
+
+	date := buildDate
+	if date == "" {
+		date = "N/A"
+	}
+
+	commit := buildCommit
+	if commit == "" {
+		commit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", version)
+	fmt.Printf("Build date: %s\n", date)
+	fmt.Printf("Build commit: %s\n", commit)
 }
