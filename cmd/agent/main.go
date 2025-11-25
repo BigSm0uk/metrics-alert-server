406e828d35532a09d77ecafa2ccfecd34b11aa6d
Denis Dekke <88782528+BigSm0uk@users.noreply.github.com>
2025-11-25 10:46:30 +0300

    Инкремент 23

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/bigsm0uk/metrics-alert-server/internal/app"
@@ -8,7 +9,15 @@ import (
 	"github.com/bigsm0uk/metrics-alert-server/internal/app/zl"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printBuildInfo()
+
 	app, err := InitializeApp()
 	if err != nil {
 		log.Fatal(err)
@@ -28,4 +37,25 @@ func InitializeApp() (*app.Agent, error) {
 	defer logger.Sync()
 
 	return app.NewAgent(cfg, logger), nil
+}
+
+func printBuildInfo() {
+	version := buildVersion
+	if version == "" {
+		version = "N/A"
+	}
+
+	date := buildDate
+	if date == "" {
+		date = "N/A"
+	}
+
+	commit := buildCommit
+	if commit == "" {
+		commit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", version)
+	fmt.Printf("Build date: %s\n", date)
+	fmt.Printf("Build commit: %s\n", commit)
 }
